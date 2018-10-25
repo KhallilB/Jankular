ea94711b0096cdea180c71bb5cd8853615049704
KhallilB <khallil.bailey@gmail.com>
2018-10-24 20:05:39 -0700

    messing with components

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,8 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: []
 })
 export class ServerComponent {
+
+    serverId = 10;
+    serverStatus = 'offline';
+
+    getServerStatus() {
+
+        return this.serverStatus;
+    }
 
 }
